d1a44f7fda4f59eb8f0d12778b50ee5f35dfd0d4
Eduardo Santos <edu.santos@superaremedia.com.br>
2019-03-11 21:42:19 -0300

    Adiciona Google Analytics ao projeto

--- wp-content/themes/eduardotema/partials/head.php
+++ wp-content/themes/eduardotema/partials/head.php
@@ -9,5 +9,12 @@
   <link href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" rel="stylesheet">
 	<link rel="stylesheet" href="<?= $home . '/css/main.css' ?>">
   <?php wp_head() ?>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-TS9KQ2D');</script>
+	<!-- End Google Tag Manager -->
 
 </head>
